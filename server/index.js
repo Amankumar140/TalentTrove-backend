a5cfd4ecf3e8b428c0134e534d0124816d4f1eaa
Aman Kumar <aman140426@gmail.com>
2025-07-19 10:50:01 +0530

    .

--- server/index.js
+++ server/index.js
@@ -35,15 +35,17 @@ io.on("connection", (socket) => {
   SocketHandler(socket);
 });
 
+app.get("/", (req, res) => {
+  res.send("TalentTrove Backend is Live!");
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.get("/", (req, res) => {
-      res.send("TalentTrove Backend is Live!");
-    });
+     
 
     app.post("/register", async (req, res) => {
       try {
